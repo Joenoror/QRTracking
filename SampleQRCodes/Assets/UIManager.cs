cbd3f2ea7e678dce46460daf2ef2522127703970
Joenoror <falanhx@gmail.com>
2023-03-29 13:37:23 +0200

    UIManager Added
    - Added UIManager Functionality

--- SampleQRCodes/Assets/UIManager.cs
+++ SampleQRCodes/Assets/UIManager.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<TMP_Text> texts;
+    public ConfigInfo configInfo;
+
+    public float frecuencyValue, speedValue, intensityValue, temperatureValue;
+
+    private void Update()
     {
-        
+        //Buscamos el archivo configinfo correspondiente al QR
+        if (configInfo == null)
+            configInfo = FindObjectOfType<ReadFromQR>().configInfo;
+        UpdateTextValue();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ConvertModbusValues() 
     {
-        
+        frecuencyValue = configInfo.modbusList[5].valueVar + (configInfo.modbusList[6].valueVar / 1000);
+        speedValue = configInfo.modbusList[7].valueVar + (configInfo.modbusList[8].valueVar / 1000);
+        intensityValue = configInfo.modbusList[9].valueVar + (configInfo.modbusList[10].valueVar / 1000);
+        temperatureValue = configInfo.modbusList[11].valueVar + (configInfo.modbusList[12].valueVar / 1000);
+    }
+
+
+    public void UpdateTextValue()
+    {
+        ConvertModbusValues();
+        texts[0].text = frecuencyValue.ToString();
+        texts[1].text = speedValue.ToString();
+        texts[2].text = intensityValue.ToString();
+        texts[3].text = temperatureValue.ToString();
     }
 
     public void ButtonResetPressed()
@@ -21,7 +41,6 @@ public class UIManager : MonoBehaviour
         Debug.Log("BOTÓN DE RESET PRESIONADO");
         //TODO
     }
-
     public void ButtonMarcha1Pressed()
     {
         Debug.Log("BOTÓN DE MARCHA_1 PRESIONADO");
